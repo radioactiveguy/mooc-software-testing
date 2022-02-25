29860cc815fad04bb0b50ae0993aec53addb2cc3
radioactiveguy <czarekw25@wp.pl>
2022-02-25 15:06:56 +0100

    LeapYear tests fixes

--- src/test/java/tudelft/numfinder/NumFinderTest.java
+++ src/test/java/tudelft/numfinder/NumFinderTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class NumFinderTest {
-    NumFinder nf;
+    private NumFinder nf;
 
     @BeforeEach
     void setUp() {
-        nf = new NumFinder();
+        this.nf = new NumFinder();
     }
 
     @Test
